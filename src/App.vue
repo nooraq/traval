4471115a761f56a2cc281c1096c9ec2cfeacae65
Nooraq <969917260@qq.com>
2020-03-25 15:24:38 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <p class="tips mb-content">
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'variable';
+.tips {
+  // font-size: $--font-size-base;
+  color: $--color-primary-light-1;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import '@/plugins/logger';
+import { DEBUG } from '@/config';
+
+import './theme/index.scss';
+// 以下三项不需要的时候可以注释掉
+import './plugins/element.js';
+import router from './router';
+import store from './store';
+
+import App from './App.vue';
+
+if (DEBUG) import('./mock');
+
+Vue.config.productionTip = false;
+
+Vue.$log.info('vue-eslint launch...');
+new Vue({
+  router,
+  store,
+  render(h) { return h(App); },
+  async mounted() {
+    this.$log.info('vue-eslint ready...');
+    setInterval(() => {
+      this.$store.commit('SYNC_CURRENT_TIME');
+    }, 1000);
+  }
+}).$mount('#app');
