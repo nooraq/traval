deb9bc72359659b2f791b4d51ee643313f212fc7
Nooraq <969917260@qq.com>
2020-05-17 01:28:08 +0800

    fix

--- src/api/demo.js
+++ src/api/demo.js
@@ -1,13 +1,7 @@
-import { Message } from 'element-ui';
 import { demo } from './adapter';
 
 export const getLocation = async (params) => {
-  try {
-    const res = await demo.get('/article', {
-      params
-    });
-    return res;
-  } catch (e) {
-    Message.error(e);
-  }
+  await demo.get('/article', {
+    params
+  });
 };
