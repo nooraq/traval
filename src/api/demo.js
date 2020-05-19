38514bd1da3f4657a8ec03423a36fff58167b541
Nooraq <969917260@qq.com>
2020-05-19 18:00:48 +0800

    fix

--- src/api/demo.js
+++ src/api/demo.js
@@ -15,9 +15,10 @@ export const getMyArticles = async params => await demo.get(
 );
 
 // post，delete，put不加括号，第二个参数直接为params
-export const postRegUser = async params => await demo.post(
-  '/regist/',
-  params
-);
-//formData类型的post第一个参数为url，调用处传params
+// eg
+// export const postRegUser = async params => await demo.post(
+//   '/regist/',
+//   params
+// );
+// post第一个参数为url，调用处传params
 export const postRegUser = post('/regist/');
