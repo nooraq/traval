7f27c46fe4be4bb552e6fc56604cb4e777d0f5d2
Sea-Qin <1545869318@qq.com>
2020-05-25 10:16:21 +0800

    isLogin error

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -3,18 +3,13 @@ export default {
     state.isLogin = 0;
     localStorage.isLogin = '0';
   },
-  // 保存最近显示的文章的id
-  changeArticleId(state, id) {
-    state.user.latestArticleId = id;
-    localStorage.latestArticleId = id;
-  },
   saveId(state, userid) {
     state.user.userid = userid;
     localStorage.userid = userid;
   },
   // 判断登录的用户账号密码是否存在
   changeLoginStatus(state, mes) {
-    state.user.isLogin = 1;
+    state.isLogin = 1;
     state.user.username = mes.username;
     localStorage.isLogin = '1';
     localStorage.username = mes.username;
